047a25f003a3f98f34d9705a4c3768d5d8e2dd87
mick-olka <mickolka64@gmail.com>
2021-04-22 13:41:07 +0300

    sampleController

--- src/sample/process/MainStep.java
+++ src/sample/process/MainStep.java
@@ -1,7 +1,5 @@
 package sample.process;
 
-import sample.controllers.TimeController;
-
 public class MainStep {
     public static void doStep() {   // основная функция вызывается каждую секунду в TimeController
         Time.tick();
