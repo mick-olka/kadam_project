3692c5d5d4a73080d695ce392647205f8804757a
mick-olka <mickolka64@gmail.com>
2021-04-28 19:37:37 +0300

    new_plan

--- src/sample/process/MainStep.java
+++ src/sample/process/MainStep.java
@@ -1,7 +1,15 @@
 package sample.process;
 
 public class MainStep {
+
+    public static Region[] world = {new Region("First"), new Region("Second"), new Region("Third")};
+
     public static void doStep() {   // основная функция вызывается каждую секунду в Controller.setTimer
         Time.tick();
     }
+
+
+
+
+
 }
---
+++ src/sample/process/Organisation.java
@@ -0,0 +1,9 @@
+package sample.process;
+
+import java.util.*;
+
+public class Organisation {
+    ArrayList<Person> group = new ArrayList<Person>();
+    ArrayList<Action> tasks = new ArrayList<Action>();
+
+}
