1766af026203c50dbc70e326171dfa743549641a
mick-olka <mickolka64@gmail.com>
2021-05-15 10:25:39 +0300

    new_readme

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -32,10 +32,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-//        ActionsPerform.readAction(1);
-//        System.out.println(ActionsPerform.tempAction.getText());
-//        System.out.println(ActionsPerform.tempAction.getPopulation());
-//        System.out.println(ActionsPerform.tempAction.getEcology());
+        //  тут можно проверять функциональную часть
         launch(args);
     }
 
